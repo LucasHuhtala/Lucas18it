237c200da405bbbdcd3c2c65e3bbceeb6e839329
Lucas Huhtala <lucas.huhtala@learnet.se>
2020-02-14 09:32:03 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace WebCamFromLib
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.cboCamera = new System.Windows.Forms.ComboBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(664, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dont press this button";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(94, 12);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(294, 232);
+            this.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pic.TabIndex = 1;
+            this.pic.TabStop = false;
+            this.pic.Visible = false;
+            this.pic.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // cboCamera
+            // 
+            this.cboCamera.FormattingEnabled = true;
+            this.cboCamera.Location = new System.Drawing.Point(494, 138);
+            this.cboCamera.Name = "cboCamera";
+            this.cboCamera.Size = new System.Drawing.Size(121, 21);
+            this.cboCamera.TabIndex = 2;
+            this.cboCamera.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(572, 177);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(216, 81);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Dont press the button";
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WebCamFromLib.Properties.Resources._1213;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 409);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cboCamera);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.ComboBox cboCamera;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Data.SqlClient;
+
+
+namespace WebCamFromLib
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap bitmap;
+        FilterInfoCollection FilterInfoCollection;
+        VideoCaptureDevice VideoCaptureDevice;
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+            VideoCaptureDevice = new VideoCaptureDevice(FilterInfoCollection[cboCamera.SelectedIndex].MonikerString);
+            VideoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+            VideoCaptureDevice.Start();
+            pic.Visible = true;
+            Image shalom = new Bitmap(@"C:\Users\lucas.huhtala\Pictures\Shalomander.jpg");
+            this.BackgroundImage = shalom;
+
+            SqlConnection con = new SqlConnection(@"Data source=CND8263QPM\LULLESQL;Initial Catalog=18IT_Test; Integrated Security=True;");
+
+            con.Open();
+            try
+            {
+                if (textBox1.Text != "")
+                {
+                    SqlCommand cmd = new SqlCommand("Select Texts From Texts", con);
+                    SqlDataReader da = cmd.ExecuteReader();
+
+                    while (da.Read())
+                    {
+                        textBox1.Text = da.GetValue(0).ToString();
+                    }
+                    con.Close();
+                }
+            }
+
+            catch
+
+            {
+                button1.Text = "Dont you dare press this button";
+                textBox1.Text = "Dont press the button";
+            }
+
+            MessageBox.Show("shalom");
+        }
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs EventArgs )
+        {
+            pic.Image = bitmap;
+            bitmap = (Bitmap)EventArgs.Frame.Clone();
+
+            Bitmap current = (Bitmap)bitmap.Clone();
+
+
+            string filepath = @"C:\Webcamtest\" + DateTime.Now.ToString("yyyy_MM_dd_hh_mm_ss") + ".png";
+            current.Save(filepath);
+
+            current.Dispose();
+
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BackGroundShalom(object sender, System.EventArgs e)
+        {
+            
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            textBox1.Text = "Dont press the button";
+
+            FilterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo FilterInfo in FilterInfoCollection)
+            cboCamera.Items.Add(FilterInfo.Name);
+            cboCamera.SelectedIndex = 0;
+
+
+
+
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
